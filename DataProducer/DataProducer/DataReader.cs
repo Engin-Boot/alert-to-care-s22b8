be62ef9b4731be6fb33b235aaf42a598f8a9a3df
ana301 <31302345+ana301@users.noreply.github.com>
2020-10-14 19:19:48 +0530

    DP tests

--- DataProducer/DataProducer/DataReader.cs
+++ DataProducer/DataProducer/DataReader.cs
@@ -1,6 +1,7 @@
 ﻿using Models;
 using Newtonsoft.Json;
 using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
@@ -9,38 +10,51 @@ namespace DataProducer
 {
     public class DataReader
     {
-        public void ReadDataAndPost() 
+        public List<IRestResponse> ReadDataAndPost()
         {
-            using (var reader = new StreamReader(@"C:\Users\320105541\OneDrive - Philips\Desktop\boot\alert-to-care-s22b8\DataProducer\vitalData.csv"))
+            try
             {
-                reader.ReadLine();
-
-                
-                while (!reader.EndOfStream)
+                using (var reader = new StreamReader(@"C:\Users\320105541\OneDrive - Philips\Desktop\boot\alert-to-care-s22b8\DataProducer\vitalData.csv"))
                 {
-                    List<PatientVitals> li = new List<PatientVitals>();
-                    for (int j = 0; j < 3; j++) {
-                        var line = reader.ReadLine();
-                        var values = line.Split(',');
-                        PatientVitals patientVitals = new PatientVitals();
-                        patientVitals.Id = int.Parse(values[0]);
-                        patientVitals.Vitals = new List<int>
+                    reader.ReadLine();
+
+                    List<IRestResponse> lr = new List<IRestResponse>();
+                    while (!reader.EndOfStream)
+                    {
+                        List<PatientVitals> li = new List<PatientVitals>();
+                        for (int j = 0; j < 3; j++)
+                        {
+                            var line = reader.ReadLine();
+                            var values = line.Split(',');
+                            PatientVitals patientVitals = new PatientVitals();
+                            patientVitals.Id = int.Parse(values[0]);
+                            patientVitals.Vitals = new List<int>
                         {
                             int.Parse(values[1]),
                             int.Parse(values[2]),
                             int.Parse(values[3])
                         };
-                        li.Add(patientVitals);
+                            li.Add(patientVitals);
+                        }
+                        var restClient = new RestClient("http://localhost:54384/api/");
+                        var restRequest = new RestRequest("VitalsAlert", Method.POST);
+
+                        restRequest.AddJsonBody(JsonConvert.SerializeObject(li));
+
+                        restClient.Execute(restRequest);
+                        IRestResponse restResponse = restClient.Execute(restRequest);
+                        lr.Add(restResponse);
+                        Thread.Sleep(10000);
+
                     }
-                    var restClient = new RestClient("http://localhost:54384/api/");
-                    var restRequest = new RestRequest("VitalsAlert", Method.POST);
-                    
-                    restRequest.AddJsonBody(JsonConvert.SerializeObject(li));
-
-                    restClient.Execute(restRequest);
-                    Thread.Sleep(10000);
-
+                    return lr;
                 }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
             }
         }
     }
